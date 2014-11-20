0a1e9a4af1710a3a98328f7867a57c77ac7e2509
Andrew Gaus <andrewgaus815@gmail.com>
2014-11-20 15:03:19 -0500

    created ADC

--- Matlab Simulation/ADC.m
+++ Matlab Simulation/ADC.m
@@ -1,1 +1,27 @@
-function [outputBits outputDB] = ADC(inputSignal)
+function [outputBinary outputDB] = ADC(inputSignal)
+
+conversion = dec2bin(uint16(inputSignal))
+
+
+% do we need to handle something when we go to 1024 and exceed the 10 bit
+% limit
+
+% flip the signal so that we can cut it easier or add zeros if needed
+signalBinary_10 = conversion(end:-1:1)
+
+if length(signalBinary_10) >= 10
+    signalBinary_10 = signalBinary_10(1:10)
+else
+    for i = length(signalBinary_10):1:10
+        signalBinary_10 = [signalBinary_10 '0']
+    end
+        
+end
+
+% flip back
+
+signalBinary_10 = signalBinary_10(end:-1:1)
+
+ouputBinary = signalBinary_10;
+output = bin2dec(signalBinary_10)
+outputDB = mag2db(output)
