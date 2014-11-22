a7b959e34e390e814d013a0bb4afbd4ab48bbcdb
Andrew Gaus <andrewgaus815@gmail.com>
2014-11-22 15:05:29 -0500

    outline for how the main file works

---
+++ Matlab Simulation/main.m
@@ -0,0 +1,13 @@
+%% main function for simulating GPS
+
+%% initialization
+
+% set up the satellite positoins and time delay
+
+%% calculate signal strength of all nearby satellites
+
+%% choose the best 4 satellites to use
+
+%% do math on the satellites chosen
+
+%% return our position
