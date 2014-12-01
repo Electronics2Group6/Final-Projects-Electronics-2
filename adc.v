973720b2aa07b0ec4b52684cce3692f71edcc700
guptar52 <guptar52@students.rowan.edu>
2014-12-01 16:03:36 -0500

    added adc
    compares input analogcompare

---
+++ adc.v
@@ -0,0 +1,26 @@
+module adc ( clk, analog1, analogcompare, dout1, trustbit);
+		input 			    clk;
+		input	  [6:0] 	analog1;// 7 bit (128) resolution
+		input	  [6:0] 	analogcompare; //same as analog1
+		
+		output	  [6:0] 	dout1;
+		output reg			trustbit; 
+		
+		reg 	  [6:0] 	dout1;		
+		reg 	  [6:0] 	dout1_reg;
+		reg 				LEG;
+
+		always @ (analogcompare)
+		begin
+		  if (analogcompare >= analog1)
+				LEG <= 1;
+		  else
+				LEG <= 0;
+		end
+		
+		always @ (posedge clk)
+		begin
+				trustbit 	<= LEG;
+				dout1 	<= dout1_reg;
+		end
+		endmodule
