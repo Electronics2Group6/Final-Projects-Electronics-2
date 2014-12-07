ec857bdea40618025abcb437f823d0a6f30c9edf
Andrew Gaus <andrewgaus815@gmail.com>
2014-12-07 11:50:27 -0500

    fixed enable for testing

--- linear_solver_test.v
+++ linear_solver_test.v
@@ -11,7 +11,7 @@ integer i = 0;
 linear_solver linear_solver(clk,x1,x2,x3,x4,y1,y2,y3,y4,z1,z2,z3,z4,r1,r2,r3,r4,c1,c2,c3,done,state,en);
 
 initial begin
-	en = 0;
+	en = 1;
 	
 	x1 = 2088202.299;
 	x2 = 11092568.240;
